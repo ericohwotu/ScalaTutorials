c0e9bfb58e4f5c844fe488fc40286b01d8faa7a0
Eric <eric.ohwotu@outlook.com>
2017-06-05 10:56:54 +0000

    initial commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,5 +4,33 @@
 object Main {
   def main(args: Array[String]): Unit = {
     println("hello world")
+
+    //define new class
+    case class Person(name: String, age: Int)
+    val person = Person("Eric", 25)
+    var name = person.name;
+    var age = person.age;
+
+    //print out the data
+    var result = isLegal(age)
+    println(s"Name: $name Age: $age")
+    println(s"$name's ability to drink is $result")
+
+    //create new child
+    val child = Person("John", 12)
+    name = child.name;
+    age = child.age;
+
+    result = isLegal(age)
+    println(s"Name: $name Age: $age")
+    println(s"$name's ability to drink is $result")
+  }
+
+  def isLegal(age: Int): Boolean = {
+      if (age>= 25){
+        true
+      }else{
+        false
+      }
   }
 }
